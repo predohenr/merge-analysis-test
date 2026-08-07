3a6e9bc3629742da16d2a74a12d5b0f0c274dfb7
Mining Worker <miningworker@gmail.com>
2026-08-07 23:01:20 +0000

    Mining Framework Analysis for merge.mergiraf.rs

--- clarity/src/vm/errors.rs
+++ clarity/src/vm/errors.rs
@@ -15,7 +15,7 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 pub use clarity_types::errors::{
-    EarlyReturnError, Error, IncomparableError, InterpreterError, InterpreterResult, RuntimeError,
+    Error, RuntimeError, InterpreterResult, EarlyReturnError, InterpreterError, IncomparableError,
 };
 
 pub use crate::vm::analysis::errors::{
