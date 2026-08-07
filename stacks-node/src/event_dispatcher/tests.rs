8d03824a11d3b73fd37581f133fa29b23350db8c
Mining Worker <miningworker@gmail.com>
2026-08-07 19:39:46 +0000

    Mining Framework Analysis for merge.mergiraf.rs

--- stacks-node/src/event_dispatcher/tests.rs
+++ stacks-node/src/event_dispatcher/tests.rs
@@ -38,10 +38,10 @@ use stacks::chainstate::stacks::{
     TransactionAuth, TransactionContractCall, TransactionPayload, TransactionPostConditionMode,
     TransactionPublicKeyEncoding, TransactionSpendingCondition, TransactionVersion,
 };
-use stacks::core::test_util::{make_unsigned_tx, to_addr};
-use stacks::core::CHAIN_ID_TESTNET;
 use stacks::net::http::HttpRequestContents;
 use stacks::net::httpcore::{send_http_request, StacksHttpRequest};
+use stacks::core::CHAIN_ID_TESTNET;
+use stacks::core::test_util::{make_unsigned_tx, to_addr};
 use stacks::types::chainstate::{
     BlockHeaderHash, StacksAddress, StacksPrivateKey, StacksPublicKey,
 };
