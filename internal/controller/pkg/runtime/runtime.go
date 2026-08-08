e8d56b5474648151d60b02f3003fcbcb78bb16e9
Mining Worker <miningworker@gmail.com>
2026-08-08 05:05:59 +0000

    Mining Framework Analysis for merge.mergiraf.go

--- internal/controller/pkg/runtime/runtime.go
+++ internal/controller/pkg/runtime/runtime.go
@@ -22,8 +22,8 @@ import (
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/utils/ptr"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"k8s.io/utils/ptr"
 
 	"github.com/crossplane/crossplane-runtime/v2/pkg/errors"
 	"github.com/crossplane/crossplane-runtime/v2/pkg/meta"
