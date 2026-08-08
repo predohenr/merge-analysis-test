2a33a933d0d7ec12f78a92876356fc7670834d8d
Mining Worker <miningworker@gmail.com>
2026-08-08 05:50:35 +0000

    Mining Framework Analysis for merge.mergiraf_semi.go

--- imagedata/errors.go
+++ imagedata/errors.go
@@ -28,3 +28,28 @@ func wrapDownloadError(err error, desc string) error {
 		ierrors.WithPrefix(fmt.Sprintf("can't download %s", desc)),
 	)
 }
+
+func newImageResponseStatusError(status int, body string) error {
+	var msg string
+
+	if len(body) > 0 {
+		msg = fmt.Sprintf("Status: %d; %s", status, body)
+	} else {
+		msg = fmt.Sprintf("Status: %d", status)
+	}
+
+	statusCode := 404
+	if status >= 400 && status < 500 {
+		statusCode = status
+	} else if status >= 500 {
+		statusCode = http.StatusBadGateway
+	}
+
+	return ierrors.Wrap(
+		ImageResponseStatusError(msg),
+		1,
+		ierrors.WithStatusCode(statusCode),
+		ierrors.WithPublicMessage(msgSourceImageIsUnreachable),
+		ierrors.WithShouldReport(false),
+	)
+}
