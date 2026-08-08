d256e7368d2ee97398b4665850a8f5422268f092
Mining Worker <miningworker@gmail.com>
2026-08-08 17:52:44 +0000

    Mining Framework Analysis for merge.mergiraf.py

--- ckan/plugins/interfaces.py
+++ ckan/plugins/interfaces.py
@@ -7,9 +7,7 @@ extend CKAN.
 from __future__ import annotations
 
 from typing import (
-    Any, Callable, IO, Iterable, Mapping, Optional, Sequence,
-    TYPE_CHECKING, Tuple, Union, List
-)
+    Any, Callable, Optional, Mapping, IO, Tuple, List, Iterable, Union, Sequence, TYPE_CHECKING, )
 
 from flask.blueprints import Blueprint
 from flask.wrappers import Response
