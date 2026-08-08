af8ac97e6c6b59143b9ec6c054a52811666a6d5a
Mining Worker <miningworker@gmail.com>
2026-08-08 10:07:36 +0000

    Mining Framework Analysis for merge.mergiraf.go

--- pkg/vif/stack.go
+++ pkg/vif/stack.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"net"
+	"runtime"
 	"net/netip"
 	"time"
 
@@ -23,7 +24,7 @@ import (
 	"github.com/telepresenceio/telepresence/v2/pkg/tunnel"
 )
 
-func NewStack(ctx context.Context, dev stack.LinkEndpoint, streamCreator tunnel.StreamCreator) (*stack.Stack, error) {
+func NewStack(ctx context.Context, ep stack.LinkEndpoint, streamCreator tunnel.StreamCreator) (*stack.Stack, error) {
 	s := stack.New(stack.Options{
 		NetworkProtocols: []stack.NetworkProtocolFactory{
 			ipv4.NewProtocol,
@@ -40,7 +41,8 @@ func NewStack(ctx context.Context, dev stack.LinkEndpoint, streamCreator tunnel.
 	if err := setDefaultOptions(s); err != nil {
 		return nil, err
 	}
-	if err := setNIC(ctx, s, dev); err != nil {
+	_, err := setNIC(s, ep)
+	if err != nil {
 		return nil, err
 	}
 	setTCPHandler(ctx, s, streamCreator)
@@ -50,11 +52,11 @@ func NewStack(ctx context.Context, dev stack.LinkEndpoint, streamCreator tunnel.
 
 const (
 	myWindowScale    = 6
-	maxReceiveWindow = 1 << (myWindowScale + 14) // 1MiB
+	maxReceiveWindow = 4 << (myWindowScale + 14) // 4MiB
 )
 
 // maxInFlight specifies the max number of in-flight connection attempts.
-const maxInFlight = 512
+const maxInFlight = 16
 
 // keepAliveIdle is used as the very first alive interval. Subsequent intervals
 // use keepAliveInterval.
@@ -76,7 +78,33 @@ func (i idStringer) String() string {
 }
 
 func setDefaultOptions(s *stack.Stack) error {
-	// Forwarding
+	sa := tcpip.TCPSACKEnabled(true)
+	if err := s.SetTransportProtocolOption(tcp.ProtocolNumber, &sa); err != nil {
+		return fmt.Errorf("SetTransportProtocolOption(tcp, TCPSACKEnabled(%t): %s", sa, err)
+	}
+
+	if runtime.GOOS == "windows" {
+		// Windows w/RACK performs poorly. ACKs do not appear to be handled in a
+		// timely manner, leading to spurious retransmissions and a reduced
+		// congestion window.
+		tr := tcpip.TCPRecovery(0)
+		if err := s.SetTransportProtocolOption(tcp.ProtocolNumber, &tr); err != nil {
+			return fmt.Errorf("SetTransportProtocolOption(tcp, TCPRecovery(%d): %s", tr, err)
+		}
+	}
+
+	// Enable Receive Buffer Auto-Tuning, see:
+	// https://github.com/google/gvisor/issues/1666
+	mo := tcpip.TCPModerateReceiveBufferOption(true)
+	if err := s.SetTransportProtocolOption(tcp.ProtocolNumber, &mo); err != nil {
+		return fmt.Errorf("SetTransportProtocolOption(tcp, TCPModerateReceiveBufferOption(%t): %s", mo, err)
+	}
+
+	cco := tcpip.CongestionControlOption("cubic")
+	if err := s.SetTransportProtocolOption(tcp.ProtocolNumber, &cco); err != nil {
+		return fmt.Errorf("SetTransportProtocolOption(tcp, CongestionControlOption(%s): %s", cco, err)
+	}
+
 	if err := s.SetForwardingDefaultAndAllNICs(ipv4.ProtocolNumber, true); err != nil {
 		return fmt.Errorf("SetForwardingDefaultAndAllNICs(ipv4, %t): %s", true, err)
 	}
@@ -93,28 +121,24 @@ func setDefaultOptions(s *stack.Stack) error {
 	return nil
 }
 
-func setNIC(ctx context.Context, s *stack.Stack, ep stack.LinkEndpoint) error {
+func setNIC(s *stack.Stack, ep stack.LinkEndpoint) (tcpip.NICID, error) {
 	nicID := s.NextNICID()
-	if err := s.CreateNICWithOptions(nicID, ep, stack.NICOptions{Name: "tel", Context: ctx}); err != nil {
-		return fmt.Errorf("create NIC failed: %s", err)
+	if tcpErr := s.CreateNIC(nicID, ep); tcpErr != nil {
+		return 0, fmt.Errorf("create NIC failed: %s", tcpErr)
 	}
 	if err := s.SetPromiscuousMode(nicID, true); err != nil {
-		return fmt.Errorf("SetPromiscuousMode(%d, %t): %s", nicID, true, err)
+		return 0, fmt.Errorf("SetPromiscuousMode(%d, %t): %s", nicID, true, err)
 	}
 	if err := s.SetSpoofing(nicID, true); err != nil {
-		return fmt.Errorf("SetSpoofing(%d, %t): %s", nicID, true, err)
+		return 0, fmt.Errorf("SetSpoofing(%d, %t): %s", nicID, true, err)
 	}
 	s.SetRouteTable([]tcpip.Route{
 		{
 			Destination: header.IPv4EmptySubnet,
 			NIC:         nicID,
 		},
-		{
-			Destination: header.IPv6EmptySubnet,
-			NIC:         nicID,
-		},
 	})
-	return nil
+	return nicID, nil
 }
 
 func forwardTCP(ctx context.Context, streamCreator tunnel.StreamCreator, fr *tcp.ForwarderRequest) {
@@ -128,59 +152,37 @@ func forwardTCP(ctx context.Context, streamCreator tunnel.StreamCreator, fr *tcp
 		}
 	}()
 
-	wq := waiter.Queue{}
+	var wq waiter.Queue
 	if ep, err = fr.CreateEndpoint(&wq); err != nil {
 		fr.Complete(true)
+		dlog.Error(ctx, err)
 		return
 	}
-	defer fr.Complete(false)
+	fr.Complete(false)
 
 	so := ep.SocketOptions()
 	so.SetKeepAlive(true)
 
 	idle := tcpip.KeepaliveIdleOption(keepAliveIdle)
 	if err = ep.SetSockOpt(&idle); err != nil {
+		dlog.Error(ctx, err)
 		return
 	}
 
 	ivl := tcpip.KeepaliveIntervalOption(keepAliveInterval)
 	if err = ep.SetSockOpt(&ivl); err != nil {
+		dlog.Error(ctx, err)
 		return
 	}
 
 	if err = ep.SetSockOptInt(tcpip.KeepaliveCountOption, keepAliveCount); err != nil {
+		dlog.Error(ctx, err)
 		return
 	}
 	dispatchToStream(ctx, newConnID(header.TCPProtocolNumber, id), gonet.NewTCPConn(&wq, ep), streamCreator)
 }
 
 func setTCPHandler(ctx context.Context, s *stack.Stack, streamCreator tunnel.StreamCreator) {
-	if err := s.SetTransportProtocolOption(tcp.ProtocolNumber,
-		&tcpip.TCPSendBufferSizeRangeOption{
-			Min:     tcp.MinBufferSize,
-			Default: tcp.DefaultSendBufferSize,
-			Max:     tcp.MaxBufferSize,
-		}); err != nil {
-		return
-	}
-
-	if err := s.SetTransportProtocolOption(tcp.ProtocolNumber,
-		&tcpip.TCPReceiveBufferSizeRangeOption{
-			Min:     tcp.MinBufferSize,
-			Default: tcp.DefaultSendBufferSize,
-			Max:     tcp.MaxBufferSize,
-		}); err != nil {
-		return
-	}
-
-	sa := tcpip.TCPSACKEnabled(true)
-	s.SetTransportProtocolOption(tcp.ProtocolNumber, &sa)
-
-	// Enable Receive Buffer Auto-Tuning, see:
-	// https://github.com/google/gvisor/issues/1666
-	mo := tcpip.TCPModerateReceiveBufferOption(true)
-	s.SetTransportProtocolOption(tcp.ProtocolNumber, &mo)
-
 	f := tcp.NewForwarder(s, maxReceiveWindow, maxInFlight, func(fr *tcp.ForwarderRequest) {
 		forwardTCP(ctx, streamCreator, fr)
 	})
