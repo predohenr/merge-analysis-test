edda6d7372106f94a4b6afc00493945a3c3bcae2
Mining Worker <miningworker@gmail.com>
2026-08-08 09:50:35 +0000

    Mining Framework Analysis for merge.mergiraf_semi.go

--- pkg/capr/planner/planner.go
+++ pkg/capr/planner/planner.go
@@ -1100,6 +1100,55 @@ func (p *Planner) generatePlanWithConfigFiles(controlPlane *rkev1.RKEControlPlan
 	return plan.NodePlan{}, map[string]interface{}{}, "", nil
 }
 
+func (p *Planner) generatePlanWithConfigFiles(controlPlane *rkev1.RKEControlPlane, tokensSecret plan.Secret, entry *planEntry, joinServer string, renderS3 bool) (plan.NodePlan, map[string]interface{}, string, error) {
+	var (
+		reg      registries
+		nodePlan plan.NodePlan
+		err      error
+	)
+
+	if !controlPlane.Spec.UnmanagedConfig {
+		nodePlan, reg, err = p.commonNodePlan(controlPlane, plan.NodePlan{})
+		if err != nil {
+			return nodePlan, map[string]interface{}{}, "", err
+		}
+		var (
+			joinedServer string
+			config       map[string]interface{}
+		)
+
+		nodePlan, config, joinedServer, err = p.addConfigFile(nodePlan, controlPlane, entry, tokensSecret, joinServer, reg, renderS3)
+		if err != nil {
+			return nodePlan, config, joinedServer, err
+		}
+
+		nodePlan, err = p.addManifests(nodePlan, controlPlane, entry)
+		if err != nil {
+			return nodePlan, config, joinedServer, err
+		}
+
+		nodePlan, err = p.addChartConfigs(nodePlan, controlPlane, entry)
+		if err != nil {
+			return nodePlan, config, joinedServer, err
+		}
+
+		nodePlan, err = addOtherFiles(nodePlan, controlPlane, entry)
+
+		idempotentScriptFile := plan.File{
+			Content: base64.StdEncoding.EncodeToString([]byte(idempotentActionScript)),
+			Path:    idempotentActionScriptPath(controlPlane),
+			Dynamic: true,
+			Minor:   true,
+		}
+
+		nodePlan.Files = append(nodePlan.Files, idempotentScriptFile)
+
+		return nodePlan, config, joinedServer, err
+	}
+
+	return plan.NodePlan{}, map[string]interface{}{}, "", nil
+}
+
 func (p *Planner) desiredPlan(controlPlane *rkev1.RKEControlPlane, tokensSecret plan.Secret, entry *planEntry, joinServer string) (plan.NodePlan, string, error) {
 	nodePlan, config, joinedTo, err := p.generatePlanWithConfigFiles(controlPlane, tokensSecret, entry, joinServer, true)
 	if err != nil {
