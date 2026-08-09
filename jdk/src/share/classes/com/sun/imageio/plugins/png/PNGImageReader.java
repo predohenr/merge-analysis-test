866979c7d709fc6132c094cd1ccdf5c06ade19ae
Mining Worker <miningworker@gmail.com>
2026-08-09 06:33:41 +0000

    Mining Framework Analysis for merge.s3m.java

--- jdk/src/share/classes/com/sun/imageio/plugins/png/PNGImageReader.java
+++ jdk/src/share/classes/com/sun/imageio/plugins/png/PNGImageReader.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000, 2025, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2000, 2013, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -624,7 +624,7 @@ public class PNGImageReader extends ImageReader {
         InputStream bais = new ByteArrayInputStream(b);
         try (InputStream iis = new InflaterInputStream(bais)) {
             return IOUtils.readNBytes(iis, MAX_INFLATED_TEXT_LENGTH);
-        }
+            }
     }
 
     private void parse_zTXt_chunk(int chunkLength) throws IOException {
