baaa70e923b78f31b8c1c4c026bd88f58edd20f5
Mining Worker <miningworker@gmail.com>
2026-08-10 14:56:52 +0000

    Mining Framework Analysis for merge.mergiraf_semi.java

--- src/java/org/apache/cassandra/utils/logging/LogbackLoggingSupport.java
+++ src/java/org/apache/cassandra/utils/logging/LogbackLoggingSupport.java
@@ -24,24 +24,33 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import ch.qos.logback.classic.turbo.ReconfigureOnChangeFilter;
 
-import com.google.common.collect.Maps;
+import ch.qos.logback.core.hook.DefaultShutdownHook;
 
-import org.apache.commons.lang3.StringUtils;
+import ch.qos.logback.classic.LoggerContext;
+
+import ch.qos.logback.classic.Logger;
+
+import ch.qos.logback.classic.spi.ILoggingEvent;
+
+import ch.qos.logback.classic.turbo.TurboFilter;
+
+import ch.qos.logback.classic.util.ContextInitializer;
+
+import ch.qos.logback.core.Appender;
+
 import org.slf4j.LoggerFactory;
 
 import org.apache.cassandra.security.ThreadAwareSecurityManager;
 
-import ch.qos.logback.classic.Level;
-import ch.qos.logback.classic.Logger;
-import ch.qos.logback.classic.LoggerContext;
-import ch.qos.logback.classic.spi.ILoggingEvent;
+import org.apache.commons.lang3.StringUtils;
+
 import ch.qos.logback.classic.spi.TurboFilterList;
-import ch.qos.logback.classic.turbo.ReconfigureOnChangeFilter;
-import ch.qos.logback.classic.turbo.TurboFilter;
-import ch.qos.logback.classic.util.ContextInitializer;
-import ch.qos.logback.core.Appender;
-import ch.qos.logback.core.hook.DefaultShutdownHook;
+
+import com.google.common.collect.Maps;
+
+import ch.qos.logback.classic.Level;
 
 /**
  * Encapsulates all logback-specific implementations in a central place.
