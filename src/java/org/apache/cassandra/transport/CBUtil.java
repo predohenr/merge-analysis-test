823f747ffab39165350d48b6892aa33e09c36750
Mining Worker <miningworker@gmail.com>
2026-08-11 05:21:51 +0000

    Mining Framework Analysis for merge.s3m.java

--- src/java/org/apache/cassandra/transport/CBUtil.java
+++ src/java/org/apache/cassandra/transport/CBUtil.java
@@ -35,6 +35,13 @@ import java.util.UUID;
 
 import com.google.common.base.Preconditions;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
+import io.netty.buffer.ByteBufUtil;
+import io.netty.buffer.PooledByteBufAllocator;
+import io.netty.buffer.UnpooledByteBufAllocator;
+import io.netty.util.concurrent.FastThreadLocal;
+
 import org.apache.cassandra.db.ConsistencyLevel;
 import org.apache.cassandra.utils.ByteArrayUtil;
 import org.apache.cassandra.utils.ByteBufferUtil;
@@ -43,13 +50,6 @@ import org.apache.cassandra.utils.Pair;
 import org.apache.cassandra.utils.TimeUUID;
 import org.apache.cassandra.utils.UUIDGen;
 import org.apache.cassandra.utils.memory.MemoryUtil;
-
-import io.netty.buffer.ByteBuf;
-import io.netty.buffer.ByteBufAllocator;
-import io.netty.buffer.ByteBufUtil;
-import io.netty.buffer.PooledByteBufAllocator;
-import io.netty.buffer.UnpooledByteBufAllocator;
-import io.netty.util.concurrent.FastThreadLocal;
 
 import static org.apache.cassandra.config.CassandraRelevantProperties.CASSANDRA_NETTY_USE_HEAP_ALLOCATOR;
 import static org.apache.cassandra.utils.LocalizeString.toUpperCaseLocalized;
