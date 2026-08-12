b7ab206211039f9d1084f4b4746ff96617cad294
Mining Worker <miningworker@gmail.com>
2026-08-12 01:10:50 +0000

    Mining Framework Analysis for merge.s3m.java

--- src/main/java/tools/jackson/databind/cfg/DefaultCacheProvider.java
+++ src/main/java/tools/jackson/databind/cfg/DefaultCacheProvider.java
@@ -20,7 +20,7 @@ public class DefaultCacheProvider
     implements CacheProvider
 {
     @Serial
-    private static final long serialVersionUID = 1L;
+private static final long serialVersionUID = 1L;
 
     private final static DefaultCacheProvider DEFAULT
         = new DefaultCacheProvider(DeserializerCache.DEFAULT_MAX_CACHE_SIZE,
