1d806f431f22ef7e17801035c326121d3892e475
Mining Worker <miningworker@gmail.com>
2026-08-15 20:24:40 +0000

    Mining Framework Analysis for merge.mergiraf.java

--- xchange-okex/src/main/java/org/knowm/xchange/okex/service/OkexAccountService.java
+++ xchange-okex/src/main/java/org/knowm/xchange/okex/service/OkexAccountService.java
@@ -1,15 +1,13 @@
 package org.knowm.xchange.okex.service;
-
-import static org.knowm.xchange.okex.OkexAdapters.adaptInstrument;
-import static org.knowm.xchange.okex.OkexAdapters.adaptTradeMode;
-import static org.knowm.xchange.okex.OkexAdapters.adaptTradingFee;
-import static org.knowm.xchange.okex.dto.OkexInstType.SPOT;
-import static org.knowm.xchange.okex.dto.OkexInstType.SWAP;
-
-import java.io.IOException;
-import java.util.HashMap;
 import java.util.List;
+import static org.knowm.xchange.okex.dto.OkexInstType.SPOT;
+import static org.knowm.xchange.okex.OkexAdapters.adaptTradingFee;
+import java.io.IOException;
 import java.util.Map;
+import java.util.HashMap;
+import static org.knowm.xchange.okex.dto.OkexInstType.SWAP;
+import static org.knowm.xchange.okex.OkexAdapters.adaptTradeMode;
+import static org.knowm.xchange.okex.OkexAdapters.adaptInstrument;
 import org.knowm.xchange.client.ResilienceRegistries;
 import org.knowm.xchange.currency.CurrencyPair;
 import org.knowm.xchange.derivative.FuturesContract;
