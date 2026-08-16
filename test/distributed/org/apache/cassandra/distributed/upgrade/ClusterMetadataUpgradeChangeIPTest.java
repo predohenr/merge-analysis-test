9b47f708e3dd7f611285a335a863b7c0bbe4d0ee
Mining Worker <miningworker@gmail.com>
2026-08-16 17:15:07 +0000

    Mining Framework Analysis for merge.mergiraf_semi.java

--- test/distributed/org/apache/cassandra/distributed/upgrade/ClusterMetadataUpgradeChangeIPTest.java
+++ test/distributed/org/apache/cassandra/distributed/upgrade/ClusterMetadataUpgradeChangeIPTest.java
@@ -29,4 +29,38 @@ public class ClusterMetadataUpgradeChangeIPTest extends ClusterMetadataUpgradeCh
         // changing all ips while upgrading
         ipChangeTestHelper();
     }
+
+    @Test
+    public void upgradeChangeIPTest() throws Throwable
+    {
+        // changing IP while upgrading node 3
+        ipChangeTestHelper(1, 2);
+    }
+
+    private void ipChangeTestHelper(int ... toUpgrade) throws Throwable
+    {
+        TokenSupplier ts = TokenSupplier.evenlyDistributedTokens(3);
+        new TestCase()
+        .nodesToUpgrade(toUpgrade)
+        .withConfig((cfg) -> cfg.with(Feature.NETWORK, Feature.GOSSIP)
+                                .set(Constants.KEY_DTEST_FULL_STARTUP, true))
+        .withBuilder(builder -> builder.withNodeIdTopology(NetworkTopology.singleDcNetworkTopology(4, "dc0", "rack0"))
+                                       .withTokenSupplier((TokenSupplier) i -> i == 4 ? ts.tokens(3) : ts.tokens(i)))
+        .nodes(3)
+        .singleUpgradeToCurrentFrom(v50)
+        .setup((cluster) -> {})
+        .runAfterClusterUpgrade((cluster) -> {
+            cluster.get(3).shutdown().get();
+            IInstanceConfig nodeConfig = cluster.newInstanceConfig();
+            nodeConfig.set("data_file_directories", cluster.get(3).config().get("data_file_directories"));
+            IUpgradeableInstance newInstance = cluster.bootstrap(nodeConfig, AbstractCluster.CURRENT_VERSION);
+            newInstance.startup();
+            cluster.get(1).nodetoolResult("cms", "initialize").asserts().success();
+
+            cluster.get(2).shutdown().get();
+            cluster.get(2).startup();
+
+            cluster.get(2).nodetoolResult("cms", "reconfigure", "3").asserts().success();
+        }).run();
+    }
 }
