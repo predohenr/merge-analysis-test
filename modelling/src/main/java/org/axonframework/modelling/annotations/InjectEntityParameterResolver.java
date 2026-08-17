46dff9fe58ebc4d95f9bdd0393f17a551381054e
Mining Worker <miningworker@gmail.com>
2026-08-17 17:21:43 +0000

    Mining Framework Analysis for merge.mergiraf_semi_plus.java

--- modelling/src/main/java/org/axonframework/modelling/annotations/InjectEntityParameterResolver.java
+++ modelling/src/main/java/org/axonframework/modelling/annotations/InjectEntityParameterResolver.java
@@ -17,8 +17,8 @@
 package org.axonframework.modelling.annotations;
 
 import jakarta.annotation.Nonnull;
-import jakarta.annotation.Nullable;
 import org.axonframework.common.configuration.Configuration;
+import jakarta.annotation.Nullable;
 import org.axonframework.messaging.Message;
 import org.axonframework.messaging.annotations.ParameterResolver;
 import org.axonframework.messaging.unitofwork.ProcessingContext;
