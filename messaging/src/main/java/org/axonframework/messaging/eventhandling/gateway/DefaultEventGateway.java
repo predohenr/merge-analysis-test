8556f78e3e4442306942cc03c5ffd0ad4e3d32d4
Mining Worker <miningworker@gmail.com>
2026-08-18 20:39:29 +0000

    Mining Framework Analysis for merge.mergiraf_semi.java

--- messaging/src/main/java/org/axonframework/messaging/eventhandling/gateway/DefaultEventGateway.java
+++ messaging/src/main/java/org/axonframework/messaging/eventhandling/gateway/DefaultEventGateway.java
@@ -15,20 +15,17 @@
  */
 
 package org.axonframework.messaging.eventhandling.gateway;
-
-import jakarta.annotation.Nonnull;
-import jakarta.annotation.Nullable;
-import org.axonframework.common.FutureUtils;
 import org.axonframework.common.infra.ComponentDescriptor;
-import org.axonframework.messaging.core.MessageTypeResolver;
-import org.axonframework.messaging.core.unitofwork.ProcessingContext;
-import org.axonframework.messaging.eventhandling.EventMessage;
-import org.axonframework.messaging.eventhandling.EventSink;
-
-import java.util.List;
 import java.util.Objects;
-import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
+import org.axonframework.messaging.eventhandling.EventMessage;
+import java.util.concurrent.CompletableFuture;
+import org.axonframework.messaging.eventhandling.EventSink;
+import java.util.List;
+import org.jspecify.annotations.Nullable;
+import org.axonframework.messaging.core.unitofwork.ProcessingContext;
+import org.axonframework.common.FutureUtils;
+import org.axonframework.messaging.core.MessageTypeResolver;
 
 /**
  * Default implementation of the {@link EventGateway} interface using the {@link EventSink} to publish events.
@@ -50,10 +47,11 @@ public class DefaultEventGateway implements EventGateway {
      * @param eventSink           the {@link EventSink} to publish events to
      * @param messageTypeResolver the {@link MessageTypeResolver} to resolve the type of the event
      */
-    public DefaultEventGateway(EventSink eventSink,
-                               MessageTypeResolver messageTypeResolver) {
-        this.eventSink = Objects.requireNonNull(eventSink, "EventSink may not be null");
-        this.messageTypeResolver = Objects.requireNonNull(messageTypeResolver, "MessageTypeResolver may not be null");
+
+    @Override
+    public void describeTo(@Nonnull ComponentDescriptor descriptor) {
+        descriptor.describeProperty("eventSink", eventSink);
+        descriptor.describeProperty("messageTypeResolver", messageTypeResolver);
     }
 
     @Override
@@ -68,9 +66,9 @@ public class DefaultEventGateway implements EventGateway {
                 : eventSink.publish(context, eventMessages);
     }
 
-    @Override
-    public void describeTo(@Nonnull ComponentDescriptor descriptor) {
-        descriptor.describeProperty("eventSink", eventSink);
-        descriptor.describeProperty("messageTypeResolver", messageTypeResolver);
+    public DefaultEventGateway(EventSink eventSink,
+                               MessageTypeResolver messageTypeResolver) {
+        this.eventSink = Objects.requireNonNull(eventSink, "EventSink may not be null");
+        this.messageTypeResolver = Objects.requireNonNull(messageTypeResolver, "MessageTypeResolver may not be null");
     }
 }
